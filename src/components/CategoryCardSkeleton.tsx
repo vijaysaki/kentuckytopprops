732112f92d095f4cf863897de3c88bc2c1ebb079
vijaysaki <40771195+vijaysaki@users.noreply.github.com>
2026-03-08 01:40:51 -0500

    Save all current changes

--- src/components/CategoryCardSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-export function CategoryCardSkeleton() {
-  return (
-    <div className="card category-card">
-      <div className="skeleton skeleton-line skeleton-title" style={{ height: "1.25rem", width: "70%" }} />
-    </div>
-  );
-}
--- src/components/ServiceCardSkeleton.tsx
+++
@@ -1,8 +0,0 @@
-export function ServiceCardSkeleton() {
-  return (
-    <div className="card">
-      <div className="skeleton skeleton-line skeleton-title" style={{ height: "1.25rem", width: "70%", marginBottom: "0.5rem" }} />
-      <div className="skeleton skeleton-line skeleton-desc" style={{ height: "0.875rem", width: "100%" }} />
-    </div>
-  );
-}
